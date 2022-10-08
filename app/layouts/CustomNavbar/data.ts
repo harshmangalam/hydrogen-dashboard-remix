6a1b0c5066dcd434b40beb2755bfb6523ec28a3b
harshmangalam <harshdev8218@gmail.com>
2022-10-08 13:11:36 +0530

    updated data

--- app/layouts/CustomNavbar/data.ts
+++ app/layouts/CustomNavbar/data.ts
@@ -1,18 +1,5 @@
-import {
-  IconHome2,
-  IconGauge,
-  IconDeviceDesktopAnalytics,
-  IconFingerprint,
-  IconCalendarStats,
-  IconUser,
-  IconSettings,
-} from "@tabler/icons";
+import { IconHome2, IconUsers } from "@tabler/icons";
 export const data = [
-  { icon: IconHome2, label: "Home" },
-  { icon: IconGauge, label: "Dashboard" },
-  { icon: IconDeviceDesktopAnalytics, label: "Analytics" },
-  { icon: IconCalendarStats, label: "Releases" },
-  { icon: IconUser, label: "Account" },
-  { icon: IconFingerprint, label: "Security" },
-  { icon: IconSettings, label: "Settings" },
+  { icon: IconHome2, label: "Home", href: "/" },
+  { icon: IconUsers, label: "Users", href: "/users" },
 ];
