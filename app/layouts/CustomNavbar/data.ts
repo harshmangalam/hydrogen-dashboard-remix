ac30c9612e8410f6dd4e4a1da08c360039dbfa86
harshmangalam <harshdev8218@gmail.com>
2022-10-08 12:33:43 +0530

    added navbar data

---
+++ app/layouts/CustomNavbar/data.ts
@@ -0,0 +1,18 @@
+import {
+  IconHome2,
+  IconGauge,
+  IconDeviceDesktopAnalytics,
+  IconFingerprint,
+  IconCalendarStats,
+  IconUser,
+  IconSettings,
+} from "@tabler/icons";
+export const data = [
+  { icon: IconHome2, label: "Home" },
+  { icon: IconGauge, label: "Dashboard" },
+  { icon: IconDeviceDesktopAnalytics, label: "Analytics" },
+  { icon: IconCalendarStats, label: "Releases" },
+  { icon: IconUser, label: "Account" },
+  { icon: IconFingerprint, label: "Security" },
+  { icon: IconSettings, label: "Settings" },
+];
