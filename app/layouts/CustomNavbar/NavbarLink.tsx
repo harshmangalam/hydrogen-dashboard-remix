ef0bca99d8268fef17e94a456697dcf90c4c6bf1
harshmangalam <harshdev8218@gmail.com>
2022-10-10 00:02:08 +0530

    added navlink

--- app/layouts/CustomNavbar/NavbarLink.tsx
+++ app/layouts/CustomNavbar/NavbarLink.tsx
@@ -1,33 +1,32 @@
-import { Tooltip, UnstyledButton } from "@mantine/core";
+import { Tooltip } from "@mantine/core";
 import useStyles from "./useStyles";
 import { TablerIcon } from "@tabler/icons";
-import { Link } from "@remix-run/react";
+import { NavLink } from "@remix-run/react";
 interface NavbarLinkProps {
   icon: TablerIcon;
   label: string;
-  active?: boolean;
   onClick?(): void;
   href: string;
 }
 
 export default function NavbarLink({
   label,
-  active,
   icon: Icon,
   onClick,
-  href
+  href,
 }: NavbarLinkProps) {
   const { classes, cx } = useStyles();
   return (
     <Tooltip label={label} position="right" transitionDuration={0}>
-      <UnstyledButton
-        component={Link}
+      <NavLink
         to={href}
         onClick={onClick}
-        className={cx(classes.link, { [classes.active]: active })}
+        className={({ isActive }) =>
+          isActive ? cx(classes.link, classes.active) : classes.link
+        }
       >
         <Icon stroke={1.5} />
-      </UnstyledButton>
+      </NavLink>
     </Tooltip>
   );
 }
