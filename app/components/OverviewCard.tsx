c2f5d4f4702d49ceb191732730dc8df04799b7c7
harshmangalam <harshdev8218@gmail.com>
2022-10-10 00:12:29 +0530

    added card icons

--- app/components/OverviewCard.tsx
+++ app/components/OverviewCard.tsx
@@ -3,12 +3,19 @@ import { Text, Paper, Group } from "@mantine/core";
 interface OverviewCardProps {
   label: string;
   count: number;
+  icon: any;
 }
 
-export default function OverviewCard({ label, count }: OverviewCardProps) {
+export default function OverviewCard({
+  label,
+  icon: Icon,
+  count,
+}: OverviewCardProps) {
   return (
     <Paper withBorder radius="md" p="xs">
-      <Group>
+      <Group align={"start"}>
+        <Icon size={24} stroke={1.5} />
+
         <div>
           <Text color="dimmed" size="xs" transform="uppercase" weight={700}>
             {label}
--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -3,6 +3,14 @@ import { json, LoaderFunction } from "@remix-run/node";
 import { useLoaderData } from "@remix-run/react";
 import OverviewCard from "~/components/OverviewCard";
 import { ADMIN_ENDPOINT } from "~/constants";
+import { HiUsers } from "react-icons/hi";
+import { FaUsers } from "react-icons/fa";
+import { BsFillFilePostFill } from "react-icons/bs";
+import {
+  RiNotificationFill,
+  RiMessage3Fill,
+  RiDeviceFill,
+} from "react-icons/ri";
 export const loader: LoaderFunction = async () => {
   try {
     const response = await fetch(`${ADMIN_ENDPOINT}`);
@@ -26,13 +34,37 @@ export default function Index() {
         { maxWidth: "xs", cols: 1, spacing: "sm" },
       ]}
     >
-      <OverviewCard label={"Users"} count={loaderData.user} />
-      <OverviewCard label={"Posts"} count={loaderData.post} />
-      <OverviewCard label={"Groups"} count={loaderData.group} />
-      <OverviewCard label={"Group Posts"} count={loaderData.groupPost} />
-      <OverviewCard label={"Notifications"} count={loaderData.notification} />
-      <OverviewCard label={"Messages"} count={loaderData.message} />
-      <OverviewCard label={"Login Devices"} count={loaderData.loginHistory} />
+      <OverviewCard
+        label={"Users"}
+        count={loaderData.user}
+        icon={HiUsers}
+      />
+      <OverviewCard
+        label={"Posts"}
+        count={loaderData.post}
+        icon={BsFillFilePostFill}
+      />
+      <OverviewCard label={"Groups"} count={loaderData.group} icon={FaUsers} />
+      <OverviewCard
+        label={"Group Posts"}
+        count={loaderData.groupPost}
+        icon={BsFillFilePostFill}
+      />
+      <OverviewCard
+        label={"Notifications"}
+        count={loaderData.notification}
+        icon={RiNotificationFill}
+      />
+      <OverviewCard
+        label={"Messages"}
+        count={loaderData.message}
+        icon={RiMessage3Fill}
+      />
+      <OverviewCard
+        label={"Login Devices"}
+        count={loaderData.loginHistory}
+        icon={RiDeviceFill}
+      />
     </SimpleGrid>
   );
 }
