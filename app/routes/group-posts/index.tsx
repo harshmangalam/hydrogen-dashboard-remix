b692de120526cb462e908afd155ee518197936e7
PranjalAgni <pranjal.agnihotri6@gmail.com>
2022-10-12 23:30:27 +0530

    :hammer: trying to fetch groupPosts

--- app/routes/group-posts/index.tsx
+++ app/routes/group-posts/index.tsx
@@ -1,4 +1,20 @@
-export default function GroupPosts() {
-    return <div>Group Posts</div>;
+import { LoaderFunction } from "@remix-run/node";
+import { useLoaderData } from "@remix-run/react";
+import { ADMIN_ENDPOINT } from "~/constants";
+
+export const loader: LoaderFunction = async () => {
+  try {
+    const response = await fetch(`${ADMIN_ENDPOINT}/groupPosts`);
+    const jsonData = await response.json();
+    console.log("Data: ", jsonData);
+    return null;
+  } catch (error) {
+    throw error;
   }
-  
+};
+
+export default function GroupPosts() {
+  const groupPosts = useLoaderData();
+  console.log({groupPosts});
+  return <div>Group Posts</div>;
+}
