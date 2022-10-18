64e6ab560e9312b2ec4f03276eb8707a79e195ed
PranjalAgni <pranjal.agnihotri6@gmail.com>
2022-10-18 12:12:31 +0530

    :rocket: added the tables

--- app/@types/post.d.ts
+++ app/@types/post.d.ts
@@ -11,6 +11,12 @@ interface Count {
   specificAudienceFriends: number;
   taggedFriends: number;
 }
+
+interface Group {
+  id: string;
+  name: string;
+}
+
 interface PostInterface {
   id: string;
   content: string;
@@ -24,5 +30,19 @@ interface PostInterface {
   author: Author;
   specificAudienceFriends: any[];
   taggedFriends: any[];
+  _count: Count;
+}
+
+interface GroupPostInterface {
+  id: string;
+  content: string;
+  authorId: string;
+  image: string;
+  isPublished: boolean;
+  groupId: string;
+  createdAt: Date;
+  updatedAt: Date;
+  author: Author;
+  group: Group;
   _count: Count;
 }
--- app/routes/group-posts/index.tsx
+++ app/routes/group-posts/index.tsx
@@ -1,3 +1,4 @@
+import { Avatar, Group, ScrollArea, Table, Text, Title } from "@mantine/core";
 import { json, LoaderFunction } from "@remix-run/node";
 import { useLoaderData } from "@remix-run/react";
 import { ADMIN_ENDPOINT } from "~/constants";
@@ -14,10 +15,55 @@ export const loader: LoaderFunction = async () => {
 
 export default function GroupPosts() {
   const groupPosts = useLoaderData();
-  const rows = groupPosts?.map((groupPost: PostInterface) => (
-    <tr key={groupPost.id}>
+  const rows = groupPosts?.map((post: GroupPostInterface) => (
+    <tr key={post.id}>
+      <td>
+        <Group spacing="sm">
+          <Avatar size={30} src={post?.author?.profileImage} radius={30} />
+          <Text size="sm" weight={500}>
+            {post.author?.firstName}
+          </Text>
+        </Group>
+      </td>
+
+      <td>
+        <Text size="sm" weight={500}>
+          {post?.group?.name}
+        </Text>
+      </td>
+      <td>
+        <Text size="sm" weight={500}>
+          {post.content}
+        </Text>
+      </td>
+      <td>
+        <Text size="sm" color="dimmed">
+          {new Date(post.createdAt).toDateString()}
+        </Text>
+      </td>
+      <td>
+        <Text size="sm" color="dimmed">
+          {new Date(post.updatedAt).toDateString()}
+        </Text>
+      </td>
     </tr>
   ));
-
-  return <div>{JSON.stringify(rows)}</div>;
+  return (
+    <ScrollArea>
+      <Title order={4}>Posts</Title>
+      <Table mt="sm" sx={{ minWidth: 800 }} verticalSpacing="sm">
+        <thead>
+          <tr>
+            <th>Author</th>
+            <th>Group</th>
+            <th>Content</th>
+            <th>Created At</th>
+            <th>Updated At</th>
+            <th />
+          </tr>
+        </thead>
+        <tbody>{rows}</tbody>
+      </Table>
+    </ScrollArea>
+  );
 }
